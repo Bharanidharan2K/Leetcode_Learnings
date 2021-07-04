f515809d2fdb10de7d18558c32890ba247eca9c4
Bharanidharan2K <bharanikkab123@gmail.com>
2021-07-04 21:40:10 +0530

    Missing_Number using Sum of natural Numbers

--- Arrays-LeetCode/src/com/Missing_Number/Main.java
+++ Arrays-LeetCode/src/com/Missing_Number/Main.java
@@ -8,11 +8,10 @@ public class Main {
 
 	}
 	public static int missingNumber(int[] nums) {
-		int xor = nums.length;
-		for(int i=0;i<nums.length;i++) {
-			xor ^= i^nums[i];
-		}
-		return xor;
+		int sum = 0;
+		for(Integer i : nums) 
+			sum += i;
+		int n = nums.length + 1;
+		return (n*(n-1)/2) - sum;
 	}
-
 }
