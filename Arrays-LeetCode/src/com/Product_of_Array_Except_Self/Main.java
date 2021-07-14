727bd44fd6a133d118d11d6fa643e3e9f47e0e82
Bharanidharan2K <bharanikkab123@gmail.com>
2021-07-14 11:00:14 +0530

    Product_of_Array_Except_Self - using O(n) time and space

--- Arrays-LeetCode/src/com/Product_of_Array_Except_Self/Main.java
+++ Arrays-LeetCode/src/com/Product_of_Array_Except_Self/Main.java
@@ -10,15 +10,19 @@ public class Main {
 	}
 	public static int[] productExceptSelf(int[] nums) {
 		int n =  nums.length;
+		int left_product[] = new int[n];
+		int right_product[] = new int[n];
 		int output_product[] = new int[n];
-		output_product[0] = 1;
-		for(int i= 1; i < n; i++) {
-			output_product[i] = nums[i-1] * output_product[i-1];
+		left_product[0] = 1;
+		right_product[n-1] = 1;
+		for(int i = 1; i < n; i++) {
+			left_product[i] = nums[i-1] * left_product[i-1];
 		}
-		int R = 1;
-		for(int i = n-1; i >= 0; i--) {
-			output_product[i] = output_product[i] * R;
-			R = R * nums[i];
+		for(int i = n-2; i >= 0; i--) {
+			right_product[i] = nums[i+1] * right_product[i+1];
+		}
+		for(int i = 0; i < n; i++) {
+			output_product[i] = left_product[i] * right_product[i];
 		}
 		return output_product;
 	}
